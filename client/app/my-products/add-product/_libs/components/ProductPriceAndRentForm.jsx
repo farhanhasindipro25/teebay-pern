76f7f11ad5bc1135d42853c88cd7296bb6274896
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-04-14 17:56:49 +0600

    feat: Added step4 form for adding products

---
+++ client/app/my-products/add-product/_libs/components/ProductPriceAndRentForm.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Button from "../../../../_libs/components/InputFields/Button";
+import TextInputField from "../../../../_libs/components/InputFields/TextInputField";
+import SelectField from "../../../../_libs/components/InputFields/SelectField";
+
+export default function ProductPriceAndRentForm({
+  currentStep,
+  setCurrentStep,
+}) {
+  return (
+    <div className="w-full space-y-2">
+      <p className="text-lg text-gray-700 text-center font-bold">
+        Name price and rent charges for your product
+      </p>
+      <div className="space-y-4">
+        <TextInputField
+          name=""
+          id=""
+          placeholder="Enter product price"
+          label="Price"
+        />
+        <div className="flex gap-4">
+          <TextInputField
+            name=""
+            id=""
+            placeholder="Enter rental charge"
+            label="Rent"
+          />
+          <SelectField
+            name=""
+            id=""
+            label="Rental Timeline"
+            placeholder="Select rental timeline"
+            options={[]}
+          />
+        </div>
+        <div className="flex gap-4 justify-end">
+          <Button
+            variant="secondary"
+            onClick={() => setCurrentStep(currentStep - 1)}
+          >
+            Go Back
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => setCurrentStep(currentStep + 1)}
+          >
+            Save & Continue
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/app/my-products/add-product/page.jsx
+++ client/app/my-products/add-product/page.jsx
@@ -4,6 +4,7 @@ import MyProductsBreadcrumb from "../_libs/components/MyProductsBreadcrumb";
 import ProductTitleForm from "./_libs/components/ProductTitleForm";
 import ProductCategoriesForm from "./_libs/components/ProductCategoriesForm";
 import ProductDescriptionForm from "./_libs/components/ProductDescriptionForm";
+import ProductPriceAndRentForm from "./_libs/components/ProductPriceAndRentForm";
 
 export default function AddProductPage() {
   const [currentStep, setCurrentStep] = useState(0);
@@ -17,6 +18,10 @@ export default function AddProductPage() {
       setCurrentStep={setCurrentStep}
     />,
     <ProductDescriptionForm
+      currentStep={currentStep}
+      setCurrentStep={setCurrentStep}
+    />,
+    <ProductPriceAndRentForm
       currentStep={currentStep}
       setCurrentStep={setCurrentStep}
     />,
