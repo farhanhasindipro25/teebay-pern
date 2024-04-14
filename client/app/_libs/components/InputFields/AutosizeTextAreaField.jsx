1396165772ca9566bab65d4a5e5b33bc8c11560c
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-04-14 15:47:29 +0600

    chore: Added reusable AutoSizeTextAreaField component

---
+++ client/app/_libs/components/InputFields/AutosizeTextAreaField.jsx
@@ -0,0 +1,44 @@
+"use client";
+import ReactTextareaAutosize from "react-textarea-autosize";
+import cn from "../../utils-kit/cn";
+import {
+  TEXTAREA_BOX_STYLES,
+  TEXTAREA_LABEL_STYLES,
+} from "../../styles-kit/AutoSizeTextAreaFieldStyleKit";
+export default function AutoSizeTextAreaField(props) {
+  const {
+    name,
+    id,
+    type = "text",
+    value,
+    label,
+    placeholder,
+    defaultValue,
+    className,
+    minRows = 1,
+    maxRows = 5,
+    onChange,
+  } = props;
+
+  const TEXTAREA_FIELD_STYLES = cn(TEXTAREA_BOX_STYLES, className);
+  return (
+    <div className="flex flex-col gap-1">
+      <label htmlFor={id} className={TEXTAREA_LABEL_STYLES}>
+        {label}
+      </label>
+      <ReactTextareaAutosize
+        placeholder={placeholder}
+        type={type}
+        minRows={minRows}
+        maxRows={maxRows}
+        name={name}
+        id={id}
+        value={value}
+        className={TEXTAREA_FIELD_STYLES}
+        defaultValue={defaultValue}
+        onChange={onChange}
+        {...props}
+      />
+    </div>
+  );
+}
---
+++ client/app/_libs/styles-kit/AutoSizeTextAreaFieldStyleKit.js
@@ -0,0 +1,4 @@
+export const TEXTAREA_LABEL_STYLES =
+  "block text-base font-semibold text-neutral-700";
+export const TEXTAREA_BOX_STYLES =
+  "resize-none pl-3 pr-8 py-2 block w-full appearance-none rounded-md border border-neutral-300 placeholder-gray-500 shadow-sm text-sm font-normal focus:border-indigo-500 focus:outline-none focus:ring-indigo-500";
