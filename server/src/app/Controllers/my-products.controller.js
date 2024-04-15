eb743235271ebbdb46f78296fa9165ccd2f737aa
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-04-15 22:31:58 +0600

    feat: Added API endpoint for products PATCH

--- server/src/app/Controllers/my-products.controller.js
+++ server/src/app/Controllers/my-products.controller.js
@@ -11,7 +11,7 @@ export const createProduct = async (req, res) => {
   } else {
     return res.json({
       status: 400,
-      message: "Categories is not in proper syntax!",
+      message: "Categories field is not in proper syntax!",
     });
   }
   const payload = {
@@ -61,6 +61,65 @@ export const getProducts = async (req, res) => {
       status: 200,
       data: products,
       message: "Retrieved all products data",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+
+export const editProduct = async (req, res) => {
+  const productId = parseInt(req.params.id);
+  const { title, categories, description, price, rent, rent_timeline } =
+    req.body;
+
+  try {
+    const existingProduct = await prisma.myProduct.findUnique({
+      where: { id: productId },
+    });
+
+    if (!existingProduct) {
+      return res.status(404).json({
+        status: 404,
+        message: "This product does not exist",
+      });
+    }
+
+    const payload = {
+      title: title,
+      description: description,
+      price: price,
+      rent: rent,
+      rent_timeline: rent_timeline,
+    };
+    if (categories) {
+      let categoryConnect = [];
+      if (Array.isArray(categories)) {
+        categoryConnect = categories.map((categoryId) => ({
+          id: categoryId,
+        }));
+      } else {
+        return res.status(400).json({
+          status: 400,
+          message: "Categories field is not in proper syntax!",
+        });
+      }
+      payload.categories = { connect: categoryConnect };
+    }
+
+    const updatedProduct = await prisma.myProduct.update({
+      where: { id: productId },
+      data: payload,
+      include: { categories: true },
+    });
+
+    return res.json({
+      status: 201,
+      data: updatedProduct,
+      message: "Product information updated!",
     });
   } catch (error) {
     return res.status(500).json({
--- server/src/app/Routes/my-products.routes.js
+++ server/src/app/Routes/my-products.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   createProduct,
+  editProduct,
   getProducts,
 } from "../Controllers/my-products.controller.js";
 
@@ -8,5 +9,6 @@ const MyproductRoutes = Router();
 
 MyproductRoutes.post("/create-product", createProduct);
 MyproductRoutes.get("/", getProducts);
+MyproductRoutes.patch("/:id", editProduct);
 
 export default MyproductRoutes;
